0de8eb1a95b78a465de0c7c75b7d475135be5104
rafakawagoe <rafakawagoe@gmail.com>
2025-11-12 18:02:51 -0300

    feat: add English and Portuguese localization files

---
+++ portifolio_rafa/src/locales/en-US.ts
@@ -0,0 +1,34 @@
+export default {
+  translation: {
+    navbar: {
+      home: "Home",
+      about: "About"
+    },
+    hero: {
+      title: "Welcome to my personal portfolio"
+    },
+    social: {
+      title: "Get in touch with me",
+      button: "Contact"
+    },
+    about: {
+      title: "Hello, my name is Rafael!",
+      description: "I am a Computer Scientist graduated from UNESP in 2022, and since 2020 I have been working with mobile and web development. I have worked with various frameworks and languages such as Flutter, React, Xamarin, Android Native, Java, JavaScript, C, C# and Python. I have developed software that is scalable, easy to maintain and used by more than 10 million users. I am very passionate about software development and try to keep myself updated on new technologies, especially those involving mobile frameworks.",
+      button: "LEARN MORE"
+    },
+    projects: {
+      title: "Projects",
+      loading: "Loading projects...",
+      productName: "Project name"
+    },
+    footer: {
+      title: "Rafael Kawagoe Gomes Muller",
+      madeWith: "Made with React",
+      contact: "Contact",
+      email: "rafael.kg.muller@unesp.br",
+      phone: "+55(11) 99192-6462",
+      linkedin: "LinkedIn",
+      github: "GitHub"
+    }
+  }
+};
---
+++ portifolio_rafa/src/locales/pt-BR.ts
@@ -0,0 +1,34 @@
+export default {
+  translation: {
+    navbar: {
+      home: "Início",
+      about: "Sobre"
+    },
+    hero: {
+      title: "Bem vindo ao meu portifólio pessoal"
+    },
+    social: {
+      title: "Entre em contato comigo",
+      button: "Contatos"
+    },
+    about: {
+      title: "Olá, meu nome é Rafael!",
+      description: "Sou um Cientista da Computação formado pela UNESP em 2022, desde 2020 venho trabalhando com desenvolvimento mobile e web, trabalhei com vários frameworks e linguagens como Flutter, React, Xamarin, Android Native, Java, JavaScript, C, C# e Python. Desenvolvi softwares que são escaláveis, fáceis de manter e usados por mais de 10 milhões de utilizadores. Sou muito apaixonado por desenvolvimento de software e tento manter-me atualizado sobre novas tecnologias, especialmente as que envolvem frameworks móveis.",
+      button: "SAIBA MAIS"
+    },
+    projects: {
+      title: "Projetos",
+      loading: "Carregando projetos...",
+      productName: "Nome do projeto"
+    },
+    footer: {
+      title: "Rafael Kawagoe Gomes Muller",
+      madeWith: "Feito com React",
+      contact: "Contato",
+      email: "rafael.kg.muller@unesp.br",
+      phone: "+55(11) 99192-6462",
+      linkedin: "LinkedIn",
+      github: "GitHub"
+    }
+  }
+};
