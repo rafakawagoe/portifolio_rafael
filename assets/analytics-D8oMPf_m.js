9a27c11febfc07dbfc5f5cedcce4df34c7bb5882
rafakawagoe <rafakawagoe@gmail.com>
2025-11-13 02:19:28 -0300

    Updates

--- assets/analytics-D8oMPf_m.js
+++
@@ -1,1 +0,0 @@
-import{a as e,c as t,i as n,n as r,o as i,r as a,s as o,t as s}from"./index-SzzIX2pA.js";export{s as trackButtonClick,r as trackEvent,a as trackLanguageChange,n as trackNavigation,e as trackPageView,i as trackScrollDepth,o as trackSocialClick,t as trackTimeOnPage};
